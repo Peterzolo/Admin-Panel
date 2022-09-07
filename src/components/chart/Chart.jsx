57f14d6623d9c83468d1c8b3b4c95d16fdf8437a
Peter Solomon <petersolomon704@gmail.com>
2022-09-07 02:10:54 +0100

    Added bar chart

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,12 +1,66 @@
-import React from 'react'
+import React from "react";
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import "./Chart.scss";
 
-import "./Chart.scss"
+const data = [
+  {
+    "month": "Jan",
+    "product_one": 4300,
+    "product_two": 3800,
+  },
+  {
+    "month": "Feb",
+    "product_one": 3000,
+    "product_two": 5043,
+  },
+  {
+    "month": "Mar",
+    "product_one": 5600,
+    "product_two": 5750,
+  },
+  {
+    "month": "Apr",
+    "product_one": 5923,
+    "product_two": 6022,
+  },
+  {
+    "month": "May",
+    "product_one": 6560,
+    "product_two": 6800,
+  },
+  {
+    "month": "Jun",
+    "product_one": 7900,
+    "product_two": 8034,
+  },
+ 
+];
+
 const Chart = () => {
   return (
-    <div className='charts-container'>
-      <h2>Chart</h2>
+    <div className="charts-container">
+      <div className="chart-title">Monthly Sales Report [ 1st half] 2022</div>
+      <BarChart width={730} height={250} data={data}>
+        {/* <CartesianGrid strokeDasharray="3 3" /> */}
+        <XAxis dataKey="month" />
+        {/* <YAxis /> */}
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="product_one" fill="#8884d8" />
+        <Bar dataKey="product_two" fill="#82ca9d" />
+      </BarChart>
     </div>
-  )
-}
+  );
+};
 
-export default Chart
+export default Chart;
