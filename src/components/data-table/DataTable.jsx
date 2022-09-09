7e1ed186d94840fbb930dd0d86a591e19823d633
Peter Solomon <petersolomon704@gmail.com>
2022-09-09 03:18:48 +0100

    modified user table

--- src/components/data-table/DataTable.jsx
+++ src/components/data-table/DataTable.jsx
@@ -2,15 +2,42 @@ import React from "react";
 import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
 import "./DataTable.scss";
 import { userColumns, userRows } from "../../data/userData";
+import { Link } from "react-router-dom";
+import DeleteIcon from "@mui/icons-material/Delete";
+import TableViewIcon from "@mui/icons-material/TableView";
 
 const DataTable = () => {
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className="cellAction">
+            <Link to="/users/test" style={{ textDecoration: "none" }}>
+              <div className="viewButton">
+                <TableViewIcon />
+              </div>
+            </Link>
+            <div
+              className="deleteButton"
+              // onClick={() => handleDelete(params.row.id)}
+            >
+              <DeleteIcon />
+            </div>
+          </div>
+        );
+      },
+    },
+  ];
   return (
     // <div style={{ height: 400, width: "100%" }}>
     <div className="userTable">
       <DataGrid
         className="container"
         rows={userRows}
-        columns={userColumns}
+        columns={userColumns.concat(actionColumn)}
         pageSize={5}
         rowsPerPageOptions={[5]}
         checkboxSelection
