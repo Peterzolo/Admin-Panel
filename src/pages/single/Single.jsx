e93fdac308d4f34713a8a92eec7067e87daa1c58
Peter Solomon <petersolomon704@gmail.com>
2022-09-09 20:17:40 +0100

    Added style

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
+import UserImage from "../../images/data1.jpg";
+import EditIcon from "@mui/icons-material/Edit";
 
 import "./Single.scss";
 
@@ -10,7 +12,20 @@ const Single = () => {
       <Sidebar />
       <div className="sub-container">
         <Navbar />
-        Single component
+        <div className="top">
+          <div className="top-left">
+            <div className="editButton">
+              <EditIcon />
+            </div>
+            <h3 className="title">User Data</h3>
+            <div className="item">
+              <img src={UserImage} alt="image" className="user-image" />
+              Details
+            </div>
+          </div>
+          <div className="top-right">Right</div>
+        </div>
+        <div className="bottom"></div>
       </div>
     </div>
   );
