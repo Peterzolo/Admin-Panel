cdbabfae456f852880178f5802d7011a64dadc24
Peter Solomon <petersolomon704@gmail.com>
2022-09-10 09:22:29 +0100

    Added new user component

--- src/pages/newPage/New.jsx
+++ src/pages/newPage/New.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import userAvatar from "../../images/user-avatar.png";
 
+import "./Newpage.scss";
 const New = () => {
   return (
-    <div>
-      <h2>New </h2>
-    </div>
-  )
-}
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        <div className="topNew">
+          <h2>Add New user</h2>
+        </div>
+        <div className="bottomNew">
+          <div className="bottom-left">
+            <img src={userAvatar} alt="user-avatar" className="avatarImage" />
+          </div>
 
-export default New
+          <div className="bottom-right">Bottom Right</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default New;
